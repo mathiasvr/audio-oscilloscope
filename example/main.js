0bfd1e34d962cc234319b78694e574d2c8467455
mathiasvr <mathiasvr@gmail.com>
2015-03-17 00:18:50 +0100

    First version

--- example/main.js
+++ example/main.js
@@ -1,18 +1,10 @@
-# audio-oscilloscope
-Waveform audio visualizer using html5 canvas.
-
-## Methods
-
- * addSignal (source:AudioNode, color)
- * start ()
- * stop ()
-
-## Example
-
-```javascript
 Oscilloscope = require('..')
 
-var context = new window.AudioContext()
+// shim
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia || navigator.msGetUserMedia
+
+var context = new (window.AudioContext || window.webkitAudioContext)()
 
 // setup canvas
 var canvas = document.querySelector('.visualizer')
@@ -22,7 +14,7 @@ canvas.height = window.innerHeight
 var options = {
   stroke: 3,		// size of the wave
   glow: 0.1,		// glowing effect
-  buffer: 1024  // buffer size ranging from 32 to 2048
+  buffer: 1024	// buffer size ranging from 32 to 2048
 }
 
 // attach oscilloscope
@@ -30,11 +22,10 @@ var oscilloscope = new Oscilloscope(canvas, options)
 
 // get user microphone
 var constraints = { video: false, audio: true };
+
 navigator.getUserMedia(constraints, function(stream) {
   var source = context.createMediaStreamSource(stream)
   oscilloscope.addSignal(source, '#00ffff')
 }, function (error) {
   console.error("getUserMedia error:", error);
 });
-
-```
