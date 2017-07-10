f86ec34464e1f3ed90c05b6b0ccccd01f3c58d39
Mathias Rasmussen <mathiasvr@gmail.com>
2017-07-10 23:50:47 +1000

    new examples

---
+++ example/custom.js
@@ -0,0 +1,58 @@
+var oscilloscope = require('..')
+
+var audioContext = new window.AudioContext()
+
+// setup canvas
+var canvas = document.createElement('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight - 50
+document.body.appendChild(canvas)
+
+// setup audio element
+var audioElement = document.createElement('audio')
+audioElement.controls = true
+audioElement.src = 'loading.mp3'
+document.body.appendChild(audioElement)
+
+// create source from html5 audio element
+var source = audioContext.createMediaElementSource(audioElement)
+
+// attach oscilloscope
+var scope = new oscilloscope(source)
+
+// reconnect audio output to speakers
+source.connect(audioContext.destination)
+
+// todo make 'start' func in osc for this default stuff
+var ctx = canvas.getContext("2d")
+ctx.lineWidth = 3
+ctx.shadowBlur = 4
+ctx.shadowColor = 'white'
+
+// custom animation loop
+function drawLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  var centerX = canvas.width / 2
+  var centerY = canvas.height / 2
+  
+  // draw circle
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, 100, 0, 2 * Math.PI, false);
+  ctx.fillStyle = 'yellow';
+  ctx.fill();
+
+  // draw three oscilloscopes in different positions and colors
+  ctx.strokeStyle = 'lime'
+  scope.draw(ctx, 0, 0, centerX, centerY)
+
+  ctx.strokeStyle = 'cyan'
+  scope.draw(ctx, centerX, 0, centerX, centerY)
+
+  ctx.strokeStyle =  'red'
+  scope.draw(ctx, 0, centerY, null, centerY)
+
+  window.requestAnimationFrame(drawLoop)
+}
+
+drawLoop()
--- example/microphone.js
+++ example/microphone.js
@@ -4,28 +4,29 @@ var oscilloscope = require('..')
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia || navigator.msGetUserMedia
 
-var context = new (window.AudioContext || window.webkitAudioContext)()
+var audioContext = new window.AudioContext()
 
 // setup canvas
-var canvas = document.querySelector('.visualizer')
+var canvas = document.createElement('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+document.body.appendChild(canvas)
 
-var options = {
-  stroke: 3,		// size of the wave
-  glow: 0.1,		// glowing effect
-  buffer: 1024	// buffer size ranging from 32 to 2048
-}
-
-// attach oscilloscope
-var scope = new oscilloscope(canvas, options)
+// customize drawing options
+var ctx = canvas.getContext("2d")
+ctx.lineWidth = 2
+ctx.strokeStyle = '#ffffff'
 
 // get user microphone
 var constraints = { video: false, audio: true };
-
 navigator.getUserMedia(constraints, function(stream) {
-  var source = context.createMediaStreamSource(stream)
-  scope.addSignal(source, '#00ffff')
+  var source = audioContext.createMediaStreamSource(stream)
+  
+  // attach oscilloscope
+  var scope = new oscilloscope(source)
+
+  // start default animation loop
+  scope.animate(ctx)
 }, function (error) {
   console.error("getUserMedia error:", error);
 });
