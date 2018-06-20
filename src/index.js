e27666c73f3b2c1b1cb01ae5d2073b0294529c3d
Mathias Rasmussen <mathiasvr@gmail.com>
2018-06-20 22:48:31 +0200

    clean up es6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default class Oscilloscope {
-  constructor (source, options = { }) {
+  constructor (source, options = {}) {
     if (!(source instanceof window.AudioNode)) {
       throw new Error('Oscilloscope source must be an AudioNode')
     }
@@ -22,7 +22,7 @@ export default class Oscilloscope {
       throw new Error('Oscilloscope animation is already running')
     }
     this.ctx = ctx
-    let drawLoop = () => {
+    const drawLoop = () => {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
       this.draw(ctx, x0, y0, width, height)
       this.drawRequest = window.requestAnimationFrame(drawLoop)
@@ -32,28 +32,24 @@ export default class Oscilloscope {
 
   // stop default signal animation
   stop () {
-    window.cancelAnimationFrame(this.drawRequest)
-    this.drawRequest = 0
-    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+    if (this.drawRequest) {
+      window.cancelAnimationFrame(this.drawRequest)
+      this.drawRequest = 0
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+    }
   }
 
   // draw signal
-  draw (ctx, x0, y0, width, height) {
-    // TODO: maybe cache values
-    x0 = x0 || 0
-    y0 = y0 || 0
-    width = width || (ctx.canvas.width - x0)
-    height = height || (ctx.canvas.height - y0)
-
+  draw (ctx, x0 = 0, y0 = 0, width = ctx.canvas.width - x0, height = ctx.canvas.height - y0) {
     this.analyser.getByteTimeDomainData(this.timeDomain)
-    let step = width / this.timeDomain.length
+    const step = width / this.timeDomain.length
 
     ctx.beginPath()
     // drawing loop (skipping every second record)
     for (let i = 0; i < this.timeDomain.length; i += 2) {
-      let percent = this.timeDomain[i] / 256
-      let x = x0 + (i * step)
-      let y = y0 + (height * percent)
+      const percent = this.timeDomain[i] / 256
+      const x = x0 + (i * step)
+      const y = y0 + (height * percent)
       ctx.lineTo(x, y)
     }
 
